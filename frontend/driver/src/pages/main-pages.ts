2a01e04752dfe017d7f14a7880b2f19344e0c924
Leonardo Quevedo <lpachecoquevedo@gmail.com>
2018-10-29 15:11:21 -0300

    refact(general): Adds general fixes to the application.

--- frontend/driver/src/pages/main-pages.ts
+++ frontend/driver/src/pages/main-pages.ts
@@ -24,58 +24,15 @@ export class AppMainPages {
     public refresh() {
         let pages = [
             {
-                getRoot: () => { return "MerchantsNearbyPage"; },
+                getRoot: () => { return "LoginPage"; },
                 getRootParams: () => { return {} },
-                getTitle: () => { return "Explorar"; },
-                getUrlPath: () => { return "home" },
-                getIcon: () => { return "compass"; },
+                getTitle: () => { return "Entrar"; },
+                getUrlPath: () => { return "app" },
+                getIcon: () => { return "log-in"; },
                 getBadge: () => { return 0; },
-                canShow: () => { return (!MarsAuthService.isLoggedIn()) || (MarsAuthService.hasRole('CUSTOMER')); },
+                canShow: () => { return !MarsAuthService.isLoggedIn(); },
                 disable: () => { return this.globals.disableNavigation }
             },
-            // Admin Pages
-            {
-                getRoot: () => { return "UsersListPage"; },
-                getRootParams: () => { return {} },
-                getTitle: () => { return "Usuários"; },
-                getUrlPath: () => { return "users" },
-                getIcon: () => { return "people"; },
-                getBadge: () => { return 0; },
-                canShow: () => { return MarsAuthService.hasRole('ADMIN') },
-                disable: () => { return this.globals.disableNavigation }
-            },
-            {
-                getRoot: () => { return "AdminSettingsPage"; },
-                getRootParams: () => { return {} },
-                getTitle: () => { return "Configurações"; },
-                getUrlPath: () => { return "settings" },
-                getIcon: () => { return "settings"; },
-                getBadge: () => { return 0; },
-                canShow: () => { return MarsAuthService.hasRole('ADMIN') },
-                disable: () => { return this.globals.disableNavigation }
-            },
-            // Customer Pages
-            {
-                getRoot: () => { return "OrderHistoryPage"; },
-                getRootParams: () => { return {} },
-                getTitle: () => { return "Meus Fretes"; },
-                getUrlPath: () => { return "orders" },
-                getIcon: () => { return "car"; },
-                getBadge: () => { return 0; },
-                canShow: () => { return MarsAuthService.hasRole('CUSTOMER') },
-                disable: () => { return this.globals.disableNavigation }
-            },
-            {
-                getRoot: () => { return "CustomerSettingsPage"; },
-                getRootParams: () => { return {} },
-                getTitle: () => { return "Configurações"; },
-                getUrlPath: () => { return "settings" },
-                getIcon: () => { return "settings"; },
-                getBadge: () => { return 0; },
-                canShow: () => { return MarsAuthService.hasRole('CUSTOMER') },
-                disable: () => { return this.globals.disableNavigation }
-            },
-            // Merchant Pages
             {
                 getRoot: () => { return "MerchantCalendarPage"; },
                 getRootParams: () => { return {} },
