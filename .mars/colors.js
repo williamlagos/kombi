52555a15b7e458463790bc021c5eafb6af603b7e
William Oliveira de Lagos <william.lagos@icloud.com>
2019-02-07 18:25:51 -0200

    Updated colors with new scheme if necessary

--- .mars/colors.js
+++ .mars/colors.js
@@ -1,5 +1,9 @@
 module.exports = {
     primary_color: "#ff6f00",
     secondary_color: "#ff9100",
-    terciary_color: "#242b35"
+    terciary_color: "#242b35",
+    // Blue scheme
+    // primary_color: "#052e7f",
+    // secondary_color: "#3460b9",
+    // terciary_color: "#3ab9f1"
 }
