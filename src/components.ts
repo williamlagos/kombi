5b6cf892b20b946df9dd69bf58c754fec342ead5
William Oliveira de Lagos <william.lagos@icloud.com>
2020-10-14 21:28:52 +0100

    Added component library to be used on React and updated parts repo

---
+++ src/components.ts
@@ -0,0 +1,26 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'component-library';
+
+
+
+export const AddressInput = /*@__PURE__*/createReactComponent<JSX.AddressInput, HTMLAddressInputElement>('address-input');
+export const AppDrawer = /*@__PURE__*/createReactComponent<JSX.AppDrawer, HTMLAppDrawerElement>('app-drawer');
+export const AppEntrance = /*@__PURE__*/createReactComponent<JSX.AppEntrance, HTMLAppEntranceElement>('app-entrance');
+export const AppMap = /*@__PURE__*/createReactComponent<JSX.AppMap, HTMLAppMapElement>('app-map');
+export const AppRoot = /*@__PURE__*/createReactComponent<JSX.AppRoot, HTMLAppRootElement>('app-root');
+export const GenericCarousel = /*@__PURE__*/createReactComponent<JSX.GenericCarousel, HTMLGenericCarouselElement>('generic-carousel');
+export const GenericWizard = /*@__PURE__*/createReactComponent<JSX.GenericWizard, HTMLGenericWizardElement>('generic-wizard');
+export const ImageUploader = /*@__PURE__*/createReactComponent<JSX.ImageUploader, HTMLImageUploaderElement>('image-uploader');
+export const PageAbout = /*@__PURE__*/createReactComponent<JSX.PageAbout, HTMLPageAboutElement>('page-about');
+export const PageAccount = /*@__PURE__*/createReactComponent<JSX.PageAccount, HTMLPageAccountElement>('page-account');
+export const PageCreate = /*@__PURE__*/createReactComponent<JSX.PageCreate, HTMLPageCreateElement>('page-create');
+export const PageOrderDetail = /*@__PURE__*/createReactComponent<JSX.PageOrderDetail, HTMLPageOrderDetailElement>('page-order-detail');
+export const PageOrderList = /*@__PURE__*/createReactComponent<JSX.PageOrderList, HTMLPageOrderListElement>('page-order-list');
+export const PageSchedule = /*@__PURE__*/createReactComponent<JSX.PageSchedule, HTMLPageScheduleElement>('page-schedule');
+export const PageSupport = /*@__PURE__*/createReactComponent<JSX.PageSupport, HTMLPageSupportElement>('page-support');
+export const PageTabs = /*@__PURE__*/createReactComponent<JSX.PageTabs, HTMLPageTabsElement>('page-tabs');
+export const RegisterWizard = /*@__PURE__*/createReactComponent<JSX.RegisterWizard, HTMLRegisterWizardElement>('register-wizard');
