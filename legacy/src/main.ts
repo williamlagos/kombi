777d7003d61bb441b768f7314bafd371fd841cc0
William Oliveira de Lagos <william.lagos@icloud.com>
2021-03-25 20:15:53 +0000

    Added new files to new structure in Ionic Angular application

---
+++ legacy/src/main.ts
@@ -0,0 +1,14 @@
+/**
+ * @license MIT
+ * @version 1.1.0
+ * @author M.A.R.S. Labs
+ * @description Application initialization module.
+ */
+
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { enableProdMode } from "@angular/core";
+import { AppModule } from "./app/app.module";
+
+enableProdMode();
+
+platformBrowserDynamic().bootstrapModule(AppModule);
