cbc9aa9cf75a2b104bfd738c769522fdc951f771
William Oliveira de Lagos <william.lagos@icloud.com>
2021-03-29 18:19:54 +0100

    Updates

---
+++ static/js/main.ce01ae2c.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpshipping=this.webpackJsonpshipping||[]).push([[0],{11:function(n,t,e){"use strict";e.r(t);var i=e(1),c=e.n(i),o=e(3),p=e.n(o),s=(e(8),e.p,e(9),e(0));var r=function(n){n&&n instanceof Function&&e.e(3).then(e.bind(null,12)).then((function(t){var e=t.getCLS,i=t.getFID,c=t.getFCP,o=t.getLCP,p=t.getTTFB;e(n),i(n),c(n),o(n),p(n)}))};p.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)("app-root",{})}),document.getElementById("root")),r()},8:function(n,t,e){},9:function(n,t,e){}},[[11,1,2]]]);
+//# sourceMappingURL=main.ce01ae2c.chunk.js.map
